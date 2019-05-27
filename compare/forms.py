dd502bcb109f1410cfdecfeecddd5389a0f20409
Liam Murphy <liammurphy@Liams-MBP-2.lan>
2019-05-27 17:31:10 +0100

    Added some bootstrap styling

--- compare/forms.py
+++ compare/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
 class CodeForm(forms.Form):
-    print("lm4")
-    deckCode1 = forms.CharField(label='Deck 1', max_length=300)
-    deckCode2 = forms.CharField(label='Deck 2', max_length=300)
+    deckCode1 = forms.CharField(label='Deck 1', max_length=300, initial="AAECAZ8FBr2GA6CAA5GAA/H+AoT8AvQFDLSbA76YA/mTA+yGA+H+Avz8ArT2 AvYHzwaTBNwDjAEA")
+    deckCode2 = forms.CharField(label='Deck 2', max_length=300, initial="AAECAZ8FCtqdA4qaA/mTA72GA6CAA5GAA/H+AsD9AoT8AvQFCrSbA+yGA+H+ Avz8ArT2AvYHzwaTBNwDjAEA")
