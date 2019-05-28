0ab3ce638295edaf7a3fcb086a1b7911944426a9
Liam Murphy <liammurphy@Liams-MBP-2.lan>
2019-05-28 18:48:24 +0100

    Path updated to match server

--- compare/newCompare.py
+++ compare/newCompare.py
@@ -94,7 +94,7 @@ def compare(code1,code2):
             else:
                 cases['0/0'].append(i)
 
-    with open('cardData', 'r') as f:
+    with open('/var/www/deckcompare/cardData', 'r') as f:
         cardData = json.load(f)
 
     output = [[],[],[]]
