1114e99ab366cc04f389eff6d65e85fe3f4289da
floris van Rijn <florisvanrijn1@gmail.com>
2019-12-26 19:45:31 +0100

    Minor changes to django views and core.py

--- compare/core.py
+++ compare/core.py
@@ -2,25 +2,25 @@ from vennComp import compare
 import jsonConvert
 import json
 
-
+#hard coded deck strings
 deckA = "AAECAaoICP4Figfv9wKZ+wLLhQPFmQPjtAPTwAMLnAKBBP8Fsgaw8ALPpQO3rQO5rQP+rgOqrwPQrwMA"
 deckB = "AAECAaoICpQD/gWKB+/3Apn7AsuFA8WZA9aZA+O0A9PAAwr/BbIGsPACjIUDtJcDt60Dua0D/q4Dqq8D0K8DAA=="
 venn = compare(deckA, deckB)
-#print(venn)
 
 with open('cardData', 'r') as f:
     #cardList is a dictionary of dictionaries
     cardList = json.load(f)
 
+
 def printNames(list):
     for i in range(2):
+        #implement exception handling incase json file is out date
         try:
             for j in range(len(list[i])):
                 cardName = cardList[str(list[i][j])]['name']
                 print(str(i+1) + "x " + cardName)
         except:
             jsonConvert.build()
-            print("Try again faggot")
 
 
 print("---------------------")
