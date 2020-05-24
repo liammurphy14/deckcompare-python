1fa63fe4f2b7b4c7a9b5631ef6690b996bf2d1b2
Liam Murphy <liammurphy@Liams-MBP-2.lan>
2020-05-24 22:37:26 +0100

    changes to form layout

--- compare/newCompare.py
+++ compare/newCompare.py
@@ -95,7 +95,7 @@ def compare(code1,code2):
                 cases['0/0'].append(i)
 
     #with open('cardData', 'r') as f:
-    with open('/var/www/deckcompare/cardData', 'r') as f:
+    with open('./cardData', 'r') as f:
         #changepath
         cardData = json.load(f)
 
